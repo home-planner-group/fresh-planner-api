d6ec70afb54040d73b72888477862a107bdbd51f
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2022-03-29 23:57:31 +0200

    add recipe DB test

--- src/test/java/utility/DataFactory.java
+++ src/test/java/utility/DataFactory.java
@@ -3,15 +3,15 @@ package utility;
 import com.freshplanner.api.database.enums.Unit;
 import com.freshplanner.api.model.authentication.RegistrationModel;
 import com.freshplanner.api.model.product.ProductModel;
+import com.freshplanner.api.model.recipe.RecipeModel;
+
+import java.util.ArrayList;
 
 /**
  * Data generator for tests.
  */
 public class DataFactory {
     public static class User {
-
-        // === USER ========================================================================================================
-
         public static RegistrationModel registration() {
             return new RegistrationModel(
                     "TestUser1",
@@ -46,6 +46,58 @@ public class DataFactory {
                     modelV1.getCarbohydrates() * 2,
                     modelV1.getProtein() * 2,
                     modelV1.getFat() * 2);
+        }
+    }
+
+    public static class Recipe {
+        public static RecipeModel recipeModelV1(Integer id) {
+            return new RecipeModel(
+                    id,
+                    "TestRecipe",
+                    "TestCategory",
+                    15,
+                    "TestDescription",
+                    1f,
+                    2f,
+                    3f,
+                    4f,
+                    new ArrayList<>()
+            );
+        }
+
+        public static RecipeModel recipeModelV2(Integer id) {
+            return new RecipeModel(
+                    id,
+                    "TestRecipeEdited",
+                    "TestCategoryEdited",
+                    30,
+                    "TestDescriptionEdited",
+                    1f,
+                    2f,
+                    3f,
+                    4f,
+                    new ArrayList<>()
+            );
+        }
+
+        public static RecipeModel.Item recipeItemV1(Integer productId) {
+            return new RecipeModel.Item(
+                    productId,
+                    "Placeholder",
+                    1f,
+                    null,
+                    "NoDescription"
+            );
+        }
+
+        public static RecipeModel.Item recipeItemV2(Integer productId) {
+            return new RecipeModel.Item(
+                    productId,
+                    "Placeholder",
+                    2f,
+                    null,
+                    "NoDescriptionEdited"
+            );
         }
     }
 }
